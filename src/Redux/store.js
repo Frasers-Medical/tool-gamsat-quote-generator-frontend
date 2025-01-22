80f4abe7a0e8d351aed2afb019cd7293f0188b70
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2025-01-22 11:34:48 +0530

    update store.js

---
+++ src/Redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
+import rootReducer from "./Reducers";
+// import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+
+const middlewareEnhancer = applyMiddleware(thunk);
+// const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+const composedEnhancers = compose(middlewareEnhancer)
+export default createStore(rootReducer, composedEnhancers);
