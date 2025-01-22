ea6e49046f912fb64875f6aedeaa683bdfc6bc49
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2025-01-22 23:03:50 +0530

    chore: delete apiClient.js

--- src/Services/apiClient.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://d3bn8hft5v4bch.cloudfront.net/";
-
-axios.defaults.headers.common["Content-Type"] = "application/json";
-
-export const APIClient = axios.create({});
