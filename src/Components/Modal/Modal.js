728f20c70964df20f6994a7f78b910bac757a3b4
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2025-01-22 11:33:23 +0530

    feat: create Modal.js

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./Modal.scss";
+import ReactModal from "react-modal";
+import CancelIcon from "@material-ui/icons/Cancel";
+import SearchBar from "../../Components/SearchBar/SearchBar";
+import PillList from "../../Components/PillList/PillList";
+
+const Modal = ({ data, onItemClick, modalState, setModalState }) => {
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    setFilteredData(data);
+  }, [data]);
+
+  const handleOnClick = (index) => {
+    onItemClick(filteredData[index]);
+  };
+
+  const renderContent = () => {
+    return (
+      <div>
+        <div className="modal">
+          <div className="modal__top-controls">
+            <CancelIcon onClick={() => setModalState(false)}></CancelIcon>
+            <SearchBar data={data} onInputChange={setFilteredData} />
+          </div>
+
+          <PillList data={filteredData} onItemClick={handleOnClick} />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <ReactModal
+      isOpen={modalState}
+      className="modal__modal"
+      overlayClassName="modal__overlay"
+      ariaHideApp={false}
+      onRequestClose={() => setModalState(false)}
+    >
+      {renderContent()}
+    </ReactModal>
+  );
+};
+
+Modal.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.string),
+  onItemClick: PropTypes.func,
+  openModal: PropTypes.func,
+};
+
+Modal.defaultProps = {
+  data: [],
+  onItemClick: (index) => index,
+  openModal: () => null,
+};
+
+export default Modal;
