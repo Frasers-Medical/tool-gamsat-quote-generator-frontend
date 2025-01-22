00b312e4d175deee75fa7b09ba5992e4a879f88f
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2025-01-22 11:31:45 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./App.scss";
-
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import QuotePage from "./Pages/QuotePage/QuotePage";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition timeout={450} classNames="fade">
+          <div>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/quote" element={<QuotePage />} />
+            </Routes>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+      <Footer />
+    </Router>
   );
 }
 
