42aa726e7267f8922a36645d77665817037493a2
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2025-01-22 11:34:27 +0530

    update GamsatTitleAndDisclaimer.js

--- src/Pages/QuotePage/Components/GamsatTitleAndDisclaimer/GamsatTitleAndDisclaimer.js
+++ src/Pages/QuotePage/Components/GamsatTitleAndDisclaimer/GamsatTitleAndDisclaimer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './GamsatTitleAndDisclaimer.scss';
-import { getGamsatTitle } from "../../Redux/selectors";
-import HelpIcon from '@mui/icons-material/Help';
+import React from "react";
+import "./GamsatTitleAndDisclaimer.scss";
+import { getGamsatTitle } from "../../../../Redux/selectors";
+import HelpIcon from "@mui/icons-material/Help";
 import { connect } from "react-redux";
 
 const GamsatTitleAndDisclaimer = ({ gamsatTitle }) => {
@@ -9,15 +9,24 @@ const GamsatTitleAndDisclaimer = ({ gamsatTitle }) => {
 
   return (
     <div>
-      { gamsatTitle ? (
+      {gamsatTitle ? (
         <div className="gamsat">
-          <h2 className="gamsat__title">{ gamsatTitle } <HelpIcon onClick={() => setDisclaimerState(!disclaimerState)} /></h2>
-          <h3 className={ disclaimerState ? "gamsat__disclaimer gamsat__disclaimer--active" : "gamsat__disclaimer" }>
+          <h2 className="gamsat__title">
+            {gamsatTitle}{" "}
+            <HelpIcon onClick={() => setDisclaimerState(!disclaimerState)} />
+          </h2>
+          <h3
+            className={
+              disclaimerState
+                ? "gamsat__disclaimer gamsat__disclaimer--active"
+                : "gamsat__disclaimer"
+            }
+          >
             Please note that the prompts generated below are not the same
             prompts presented in the specified Graduate Medical School
             Admissions Test (GAMSAT). They are instead intended to reflect
             similar themes, ideas, and concerns of the prompts of that
-            particular test. This is in line with ACER's copyright misconduct
+            particular test. This is in line with ACER&apos;s copyright misconduct
             regulations which protects all GAMSAT material from being reproduced
             and distributed.
           </h3>
@@ -27,7 +36,7 @@ const GamsatTitleAndDisclaimer = ({ gamsatTitle }) => {
       )}
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   const gamsatTitle = getGamsatTitle(state);
@@ -38,5 +47,5 @@ const mapDispatchToProps = null;
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(GamsatTitleAndDisclaimer);
