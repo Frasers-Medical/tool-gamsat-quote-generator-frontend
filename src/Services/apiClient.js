1caeebb74bcdbbca0ff635b53451ae21da0b751c
frasersMedical <159877463+frasersMedical@users.noreply.github.com>
2025-03-04 13:02:15 +1100

    Update apiClient.js
    Update backend url for standardised naming in Heroku

--- src/Services/apiClient.js
+++ src/Services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  "https://tool-gamsat-quote-generator-be-9e635c5f22b4.herokuapp.com/";
+  "https://tool-gamsatquotegenerator-be-47ee72575a55.herokuapp.com/";
 
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
